382a48a77194d2144acea761403ad8bdb09dcdac
Daniel Posthuma <dnjp@posteo.org>
2021-07-05 19:06:12 -0400

    reset data.c

--- src/cmd/9term/data.c
+++ src/cmd/9term/data.c
@@ -175,7 +175,6 @@ Cursor *corners[9] = {
 void
 iconinit(void)
 {
-	/* background = allocimage(display, Rect(0,0,1,1), RGB24, 1, 0x868787FF); */
-	background = display->black;
+	background = allocimage(display, Rect(0,0,1,1), RGB24, 1, 0x868787FF);
 	red = allocimage(display, Rect(0,0,1,1), RGB24, 1, 0xDD0000FF);
 }
